bc3d12e545ac280d6a1096055cbf4c402380ab2f
Roman Valls Guimera <brainstorm@users.noreply.github.com>
2023-01-17 16:39:15 +1100

    Applying the AWS docs on https://docs.aws.amazon.com/sdk-for-rust/latest/dg/testing.html as-is... where is the list_objects_impl method?

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,98 @@
 use std::error::Error;
+use std::str::FromStr;
 
+use async_trait::async_trait;
 use aws_config;
 use aws_sdk_s3::{Client};
 use aws_sdk_s3 as s3;
 
-// pub struct ListObjectsResult {
-//     pub objects: Vec<s3::model::Object>,
-//     pub continuation_token: Option<String>,
-//     pub has_more: bool,
-// }
+pub struct ListObjectsResult {
+    pub objects: Vec<s3::model::Object>,
+    pub continuation_token: Option<String>,
+    pub has_more: bool,
+}
 
-// #[async_trait]
-// pub trait ListObjects {
-//     async fn list_objects(
-//         &self,
-//         bucket: &str,
-//         prefix: &str,
-//         continuation_token: Option<String>,
-//     ) -> Result<ListObjectsResult, Box<dyn Error + Send + Sync + 'static>>;
-// }
+#[async_trait]
+pub trait ListObjects {
+    async fn list_objects(
+        &self,
+        bucket: &str,
+        prefix: &str,
+        continuation_token: Option<String>,
+    ) -> Result<ListObjectsResult, Box<dyn Error + Send + Sync + 'static>>;
+}
+
+#[derive(Clone, Debug)]
+pub struct S3ListObjects {
+    s3: s3::Client,
+}
+
+impl S3ListObjects {
+    pub fn new(s3: s3::Client) -> Self {
+        Self { s3 }
+    }
+}
+
+#[async_trait]
+impl ListObjects for S3ListObjects {
+    async fn list_objects(
+        &self,
+        bucket: &str,
+        prefix: &str,
+        continuation_token: Option<String>,
+    ) -> Result<ListObjectsResult, Box<dyn Error + Send + Sync + 'static>> {
+        let response = self
+            .s3
+            .list_objects_v2()
+            .bucket(bucket)
+            .prefix(prefix)
+            .set_continuation_token(continuation_token)
+            .send()
+            .await?;
+        Ok(ListObjectsResult {
+            objects: response.contents().unwrap_or_default().to_vec(),
+            continuation_token: response.continuation_token().map(|t| t.to_string()),
+            has_more: response.is_truncated(),
+        })
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct TestListObjects {
+    expected_bucket: String,
+    expected_prefix: String,
+    pages: Vec<Vec<s3::model::Object>>,
+}
+
+#[async_trait]
+impl ListObjects for TestListObjects {
+    async fn list_objects(
+        &self,
+        bucket: &str,
+        prefix: &str,
+        continuation_token: Option<String>,
+    ) -> Result<ListObjectsResult, Box<dyn Error + Send + Sync + 'static>> {
+        assert_eq!(self.expected_bucket, bucket);
+        assert_eq!(self.expected_prefix, prefix);
+
+        let index = continuation_token
+            .map(|t| usize::from_str(&t).expect("valid token"))
+            .unwrap_or_default();
+        if self.pages.is_empty() {
+            Ok(ListObjectsResult {
+                objects: Vec::new(),
+                continuation_token: None,
+                has_more: false,
+            })
+        } else {
+            Ok(ListObjectsResult {
+                objects: self.pages[index].clone(),
+                continuation_token: Some(format!("{}", index + 1)),
+                has_more: index + 1 < self.pages.len(),
+            })
+        }
+    }
+}
 
 // Lists all objects in an S3 bucket with the given prefix, and adds up their size.
 async fn determine_prefix_file_size(
@@ -29,12 +103,8 @@ async fn determine_prefix_file_size(
     let mut next_token: Option<String> = None;
     let mut total_size_bytes = 0;
     loop {
-        let response = s3
-            .list_objects_v2()
-            .bucket(bucket)
-            .prefix(prefix)
-            .set_continuation_token(next_token.take())
-            .send()
+        let response = list_objects_impl
+            .list_objects(bucket, prefix, next_token.take())
             .await?;
 
         // Add up the file sizes we got back
@@ -51,6 +121,28 @@ async fn determine_prefix_file_size(
         }
     }
     Ok(total_size_bytes)
+}
+#[tokio::test]
+async fn test_single_page() {
+    use s3::model::Object;
+
+    // Create a TestListObjects instance with just one page of two objects in it
+    let fake = TestListObjects {
+        expected_bucket: "some-bucket".into(),
+        expected_prefix: "some-prefix".into(),
+        pages: vec![[5, 2i64]
+            .iter()
+            .map(|size| Object::builder().size(*size).build())
+            .collect()],
+    };
+
+    // Run the code we want to test with it
+    let size = determine_prefix_file_size(&fake, "some-bucket", "some-prefix")
+        .await
+        .unwrap();
+
+    // Verify we got the correct total size back
+    assert_eq!(7, size);
 }
 
 #[tokio::main]
